14f5789437bd4230b31f2bab578e84fd7d5d19b2
Yavuz <yavuzkaand@hotmail.com>
2025-12-10 15:24:03 +0100

    rendering cards into the content

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const headerTitle = document.querySelector('.header-title');
 // able to render the card workouts
 const renderWorkoutCard = (name, image, reps) => {
   console.log(name, image, reps);
+
+  // Creating card element
+  let cardElement = document.createElement('div');
+  cardElement.className = 'card';
+
+  // Create image element
+  let imageElement = document.createElement('img');
+  imageElement.src = image;
+
+  // Create title element
+  let workoutTitle = document.createElement('h2');
+  workoutTitle.className = 'title';
+  workoutTitle.textContent = name;
+
+  // Create a description element {style it into a h4} (dont forget to add the data into the json)
+
+  // Create a reps section in the bottom {style into a p}
+
+  // Add the elements into the card
+  cardElement.append(imageElement);
+  cardElement.append(workoutTitle);
+
+  // Add the card elements into the content section
+  contentElement.append(cardElement);
 };
 
 // Fetching the workouts data
